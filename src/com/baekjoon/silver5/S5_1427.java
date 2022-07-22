fb082145f65d15a2321607a57a712c6effd1fde3
LeeJun <yuog12@naver.com>
2022-07-22 16:58:37 +0900

    [ 2022. 07. 22 ] 시간단축 : 208ms -> 124ms

--- src/com/baekjoon/silver5/S5_1427.java
+++ src/com/baekjoon/silver5/S5_1427.java
@@ -1,25 +1,27 @@
 package com.baekjoon.silver5;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class S5_1427 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
-        Scanner scan = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        String input = scan.nextLine();
-
-        scan.close();
+        String input = br.readLine();
 
         char[] arr = input.toCharArray();
 
         Arrays.sort(arr);
 
         for(int i=arr.length-1; i>=0; i--) {
-            System.out.print(arr[i]);
+            sb.append(arr[i]);
         }
+
+        System.out.println(sb.toString());
 
     }
 
