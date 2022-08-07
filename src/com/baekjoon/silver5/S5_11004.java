da6e22c9e8d540da0ececc5d4e16dcce2f7fbfd5
LeeJun <yuog12@naver.com>
2022-08-07 17:57:59 +0900

    [ 2022. 08. 07 ] 시간 초과 문제 BufferedReader + Collections.sort 사용하여 해결 ( 기존에는 Scanner + Arrays.sort )

--- src/com/baekjoon/silver5/S5_11004.java
+++ src/com/baekjoon/silver5/S5_11004.java
@@ -1,27 +1,30 @@
 package com.baekjoon.silver5;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class S5_11004 {
     
     // quick Selection 사용 해야 함.
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
-        Scanner scan = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int n = scan.nextInt();
-        int k = scan.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         List<Integer> list = new ArrayList<>();
 
-        for(int i=0; i<n; i++) {
-            list.add(scan.nextInt());
-        }
+        st = new StringTokenizer(br.readLine()," ");
 
-        scan.close();
+        while(st.hasMoreTokens()) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
 
         Collections.sort(list);
 
