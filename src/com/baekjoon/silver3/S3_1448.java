5faca1be36c70afeda8ac266f4a7ad1d8cc99f3c
LeeJun <yuog12@naver.com>
2022-08-22 16:53:10 +0900

    [ 2022. 08. 22 ] for 문 2번 사용 시 시간 초과 발생

--- src/com/baekjoon/silver3/S3_1448.java
+++ src/com/baekjoon/silver3/S3_1448.java
@@ -1,29 +1,38 @@
 package com.baekjoon.silver3;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class S3_1448 {
 
-    // 보류
-    public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = scan.nextInt();
-        int[] input = new int[n];
+        List<Integer> list = new ArrayList<>();
 
-        for(int i = 0; i < n; i++) {
-            input[i] = scan.nextInt();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
         }
 
-        scan.close();
-
-        Arrays.sort(input);
+        Collections.sort(list, Collections.reverseOrder());
 
         int result = 0;
 
-        System.out.println(result);
+        for(int i=0; i<n-2; i++) {
+            int sum = list.get(i+1) + list.get(i+2);
+            if(list.get(i) < sum) {
+                result = Math.max(result, sum + list.get(i));
+            }
+        }
+
+        System.out.println(result==0?-1:result);
+
     }
     
 }
