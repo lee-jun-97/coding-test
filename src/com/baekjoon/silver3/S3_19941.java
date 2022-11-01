32af7e61f82488c3c6f68e808ca13361974df3fc
LeeJun <yuog12@naver.com>
2022-11-01 20:34:03 +0900

    ** Greedy

--- src/com/baekjoon/silver3/S3_19941.java
+++ src/com/baekjoon/silver3/S3_19941.java
@@ -1,27 +1,33 @@
 package com.baekjoon.silver3;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
 
 public class S3_19941 {
 
-    // 포기
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
-        Scanner scan = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        int n = scan.nextInt();
-        int k = scan.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        String input = scan.next();
-
-        scan.close();
+        char[] input = br.readLine().toCharArray();
 
         int result = 0;
 
-        for(int i = 0; i < n; i++) {
-            if (input.charAt(i) == 'H') {
-                for(int j = 0; j < k; j++) {
-                    if(input.charAt(i+j) == 'P') {
+        for(int i=0; i<n; i++) {
+            if(input[i] == 'P') {
+
+                int start = i-k<0?0:i-k;
+                int end = i+k>n-1?n-1:i+k;
+
+                for(int j=start; j<=end; j++) {
+                    if(input[j] == 'H') {
+                        input[j] = '-';
                         result++;
                         break;
                     }
