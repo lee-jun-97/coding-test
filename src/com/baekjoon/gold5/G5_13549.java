615a7b5de0d72486dc2bbfbaea28eaee7139037f
LeeJun <yuog12@naver.com>
2022-11-10 16:52:48 +0900

    Problem that could not be solved

--- src/com/baekjoon/gold5/G5_13549.java
+++ src/com/baekjoon/gold5/G5_13549.java
@@ -7,41 +7,69 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class G5_13549 {
+
+    public static class Node {
+        int a;
+        int b;
+
+        public Node(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    static Queue<Node> queue = new LinkedList<>();
+    static boolean[] visited = new boolean[100001];
+
+    static int n;
+    static int k;
+    static int max = 100000;
     
     public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        n  = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new Node(n, 0));
+        visited[n] = true;
 
-        int result = 0;
+        bfs();
 
-        queue.add(new int[] {n, 0});
+    }
 
-        while(true) {
-            int[] temp = queue.poll();
+    static void bfs() {
 
-            if(temp[0] == k) {
-                result = temp[1];
+        int min = Integer.MAX_VALUE;
 
-                for(int[] arr : queue) {
-                    if(arr[0] == k) {
-                        result = Math.min(result, arr[1]);
-                    }
-                }
+        while(!queue.isEmpty()) {
 
-                queue.clear();
-                break;
-            } else {
-                queue.add(new int[] { temp[0] + 1, temp[1] + 1 });
-                queue.add(new int[] { temp[0] - 1, temp[1] + 1 });
-                queue.add(new int[] { temp[0] * 2, temp[1] });
+            Node temp = queue.poll();
+
+            int loc = temp.a;
+            int count = temp.b;
+
+            visited[loc] = true;
+
+            if(loc == k) {
+                min = Math.min(min, count);
+            }
+
+            if(loc*2 <= max && !visited[loc * 2]) {
+                queue.offer(new Node(loc * 2, count));
+            }
+
+            if(loc + 1 <= max && !visited[loc + 1]) {
+                queue.offer(new Node(loc + 1, count + 1));
+            }
+
+            if(loc - 1 >= 0 && !visited[loc - 1]) {
+                queue.offer(new Node(loc - 1, count + 1));
             }
         }
-        System.out.println(result);
+
+        System.out.println(min);
     }
 }
